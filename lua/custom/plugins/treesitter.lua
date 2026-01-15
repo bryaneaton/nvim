fa659a04db7fbc6ff2a503468b6f94753fbb0235
Bryan Eaton <bryan.eaton77@gmail.com>
2026-01-15 09:49:30 -0500

    fix treesitter

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,42 +1,55 @@
 return {
-  { -- Highlight, edit, and navigate code
+  {
+    -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
     build = ':TSUpdate',
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     config = function()
-      -- Install languages
-      require('nvim-treesitter').install({ 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python' })
+      require('nvim-treesitter.configs').setup {
+        -- Parsers to install
+        ensure_installed = {
+          'bash',
+          'c',
+          'diff',
+          'html',
+          'lua',
+          'luadoc',
+          'markdown',
+          'markdown_inline',
+          'query',
+          'vim',
+          'vimdoc',
+          'python',
+        },
 
-      -- Enable treesitter highlighting for supported filetypes
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = { 'bash', 'c', 'diff', 'html', 'lua', 'markdown', 'python', 'vim' },
-        callback = function() vim.treesitter.start() end,
-      })
+        -- Syntax highlighting
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
 
-      -- Enable treesitter-based folding
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = { 'bash', 'c', 'diff', 'html', 'lua', 'markdown', 'python', 'vim' },
-        callback = function()
-          vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-          vim.wo.foldmethod = 'expr'
-        end,
-      })
+        -- Treesitter-based indentation
+        indent = {
+          enable = true,
+          -- disable = { 'python' }, -- uncomment if python indent causes issues
+        },
 
-      -- Enable treesitter-based indentation where supported
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = { 'bash', 'c', 'html', 'lua', 'python', 'vim' },
-        callback = function()
-          vim.bo.indentexpr = 'v:lua.vim.treesitter.indentexpr()'
-        end,
-      })
+        -- Treesitter-based folding
+        fold = {
+          enable = true,
+        },
+
+        -- Incremental selection (optional but recommended)
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = 'gnn',
+            node_incremental = 'grn',
+            scope_incremental = 'grc',
+            node_decremental = 'grm',
+          },
+        },
+      }
     end,
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 }
-
