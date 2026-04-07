8ea771631d7dbd49fc0d567f43819a006f6016ff
Bryan Eaton <bryan.eaton77@gmail.com>
2026-04-07 11:36:18 -0400

    update to main branch, treesitter.

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     lazy = false,
     build = ':TSUpdate',
     config = function()
